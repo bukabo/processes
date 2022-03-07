87b3ef353226e19d80a3985eccf9fbd5cee86344
lenovo_notebook <merc.tm@gmail.com>
2022-03-08 01:51:05 +0300

    init

---
+++ main.py
@@ -0,0 +1,57 @@
+import psutil
+
+
+# Iterate over all running process
+
+
+def proesses():
+    for proc in psutil.process_iter():
+        try:
+            # Get process name & pid from process object.
+            processName = proc.name()
+            processID = proc.pid
+            print(processName, ' ::: ', processID)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
+def list_Of_Process_Names():
+    listOfProcessNames = list()
+    # Iterate over all running processes
+    for proc in psutil.process_iter():
+        # Get process detail as dictionary
+        pInfoDict = proc.as_dict(attrs=['pid', 'name', 'cpu_percent'])
+        # Append dict of process detail in list
+        listOfProcessNames.append(pInfoDict)
+
+    # return filter(listOfProcessNames, lambda x: x[]
+    return listOfProcessNames
+
+
+def ListOfProcessSortedByMemory():
+    '''
+    Get list of running process sorted by Memory Usage
+    '''
+    listOfProcObjects = []
+    # Iterate over the list
+    for proc in psutil.process_iter():
+        try:
+            # Fetch process details as dict
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+            pinfo['vms'] = proc.memory_info().vms / (1024 * 1024)
+            # Append dict to list
+            listOfProcObjects.append(pinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    # Sort list of dict by key vms i.e. memory usage
+    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
+    return listOfProcObjects
+
+
+if __name__ == '__main__':
+    # proesses()
+    print(list_Of_Process_Names())
+
+    #
+    for elem in list_Of_Process_Names():
+        print(elem)
